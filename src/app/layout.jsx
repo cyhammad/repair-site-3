5e355e7b5b70e6de764d2b085eb141a0db41ff2a
usama iftikhar <usamaiftikhar718@gmail.com>
2024-10-14 12:47:39 +0500

    gtag added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,6 +7,8 @@ import { phone } from "@/lib/phone";
 import CallAndWhatsappButton from "@/components/buttons/CallAndWhatsappButton";
 import Footer from "@/components/footer/Footer";
 import FixedButtons from "@/components/buttons/FixedButtons";
+import Script from "next/script";
+
 
 const font = Afacad({
   subsets: ["latin"],
@@ -21,7 +23,38 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+      <html lang="en">
+      <head>
+        {/* Include gtag.js script */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16714947224" />
+
+        {/* Include gtag.js initialization script */}
+        <Script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16714947224');
+        ` }} />
+      
+        <Script dangerouslySetInnerHTML={{ __html: `
+          function gtag_report_conversion(url) {
+            var callback = function () {
+              if (typeof(url) != 'undefined') {
+                window.location = url;
+              }
+            };
+            gtag('event', 'conversion', {
+              'send_to': 'AW-11336173490/jybtCK6dgcQZELKPwZ0q',
+              'event_callback': callback
+            });
+            return false;
+          }
+        ` }} />
+
+        {/* Other head elements... */}
+        {/* Google Tag Manager - Global base code */}
+      
+      </head>
       <body
         className={`${font.className} flex w-full flex-col items-center justify-center`}
       >
